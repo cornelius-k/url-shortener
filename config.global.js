3961dce07232b412b43f465ee412f9f6857d2b6f
Neil Kempin <neilkempin@gmail.com>
2016-09-06 13:49:01 -0400

    create global config file

---
+++ config.global.js
@@ -0,0 +1,16 @@
+/*
+* Set up global config
+*/
+module.exports = {
+  mongoURL: 'mongodb://localhost:27017',
+  dbName: '/hz'
+};
+
+//allow config to be overriden by dev environment-specifc config file
+if(process.env.NODE_ENV == 'development'){
+  var devConfig = require('config.development');
+  var keys = Object.keys(devConfig);
+  keys.forEach(function(key){
+    module.exports[key] = dev.config[key];
+  });
+}
