b133493433fea5aa8e8e46674081371cc1542e47
Neil Kempin <neilkempin@gmail.com>
2016-09-07 19:34:12 -0400

    reorganize and implement CRUD URL Service functions
    - move shortening algorithms into module.exports
    - fix incorrect usage of default in shorten function's switch case
    - improve saveRedirect to return mongo results set rather than entire mongo object response
    - create getRedirectForKey to return the redirect value for a given key
    - create removeRedirectWithKey to remove a redirect record by key

--- services/url-service.js
+++ services/url-service.js
@@ -5,35 +5,49 @@
 var appRoot = require('app-root-path');
 var config = require(appRoot + '/config.global');
 var MongoClient = require('mongodb').MongoClient;
+var validURL = require('valid-url');
+var nodeURL = require('url');
 
-//shortening algorithms
-function shortenBasicHash(str){
-  //credit http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
-  var hash = 0, i, chr, len;
-  if (str.length === 0) return hash;
-  for (i = 0, len = str.length; i < len; i++) {
-    chr   = str.charCodeAt(i);
-    hash  = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
-}
 
-function shortenVowless(str){
-  return str;
-}
+
+
 
 module.exports = {
 
+  //shortening algorithms
+  basicHash: function(str){
+    //credit http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+    var hash = 0, i, chr, len;
+    if (str.length === 0) return hash;
+    for (i = 0, len = str.length; i < len; i++) {
+      chr   = str.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  },
+
+  shortenBasicHash: function(str){
+    var hash = this.basicHash(str);
+    var hashedStr = hash.toString();
+    hashedStr = hashedStr.substr(0, 4);
+    return hashedStr;
+  },
+
+  shortenVowless : function(str){
+    return str;
+  },
+
   shorten : function (url, style = ''){
     var shortened;
-
+    style = style || 'default';
     switch(style){
       case "vowless":
-        shortened = shortenVowless(url);
+        shortened = this.shortenVowless(url);
         break;
-      default:
-        shortened = shortenBasicHash(url);
+      case "default":
+        shortened = this.shortenBasicHash(url);
+        break;
     }
 
     return shortened;
@@ -41,17 +55,49 @@ module.exports = {
 
   cleanURL : function(url){
     var protocol = nodeURL.parse(url).protocol;
-    if(protocol == nul)
+    if(protocol == null)
       url = "http://" + url;
     return url;
   },
 
+  isValidURL : function(url){
+    return validURL.isUri(url) ? true : false;
+  },
+
+  //CRUD Operations
   saveRedirect: function(redirect, done){
     MongoClient.connect(config.mongoURL + config.dbName, (err, db) => {
       if (err) console.log(err);
 
       db.collection(config.dbCollectionName).insert(redirect, (err, result) => {
         if(err) console.log(err);
+        //dig into mongo result set
+        result = result.ops[0];
+        done(err, result);
+      });
+
+      db.close();
+    });
+  },
+
+  getRedirectForKey: function(key, done){
+    MongoClient.connect(config.mongoURL + config.dbName, function(err, database) {
+      if (err) console.log(err);
+      db = database;
+      db.collection(config.dbCollectionName).findOne({key: key}, function(err, result) {
+        if(err) console.log(err);
+        done(err, result);
+      });
+    });
+  },
+
+  removeRedirectWithKey: function(key, done){
+    MongoClient.connect(config.mongoURL + config.dbName, (err, db) => {
+      if (err) console.log(err);
+
+      db.collection(config.dbCollectionName).remove({key: key}, (err, result) => {
+        if(err) console.log(err);
+        debugger;
         done(err, result);
       });
     });
