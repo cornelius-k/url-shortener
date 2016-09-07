f421639e66852cbd925702c58b154e70696155d9
Neil Kempin <neilkempin@gmail.com>
2016-09-07 17:18:17 -0400

    add tests and improve URL API test suite
    - test description wording improved
    - actual assertions more concise
    - fix bug where REST api tests were incorrectly saving a key as "undefined"
    - fix redirect deletion REST api test which was using the wrong path to look up a redirect

--- test/url-test.js
+++ test/url-test.js
@@ -17,35 +17,42 @@ describe('Shortening Functions', function(){
 
   var longURL = 'http://zombo.com/its/zombo/com';
   var longURLNoHTTP = 'zombo.com/its/zombo/com';
-  it('does basic string shortening', function(done){
+  it('generates a valid basic shortened string', function(done){
     var shortened = URLService.shorten(longURL);
-    expect(shortened).to.not.be.null;
-    expect(shortened == longURL).to.not.be.true;
+    expect(shortened).to.be.a.string;
+    expect(shortened).to.not.equal(longURL);
     done();
   });
 
-  it('shortens via vowless method', function(done){
+  it('generates a shortened string via vowless style', function(done){
       var shortened = URLService.shortenVowless(longURL);
       expect(shortened === 'zmbtszmbcm');
       done();
   });
 
   it('cleans urls by ensuring they have an http protocol prefix', function(done){
-      var cleaned = URLservice.cleanURL(longURLNoHTTP);
+      var cleaned = URLService.cleanURL(longURLNoHTTP);
       expect(longURLNoHTTP === 'http://' + longURLNoHTTP);
       done()
   });
 
   it('validates urls are formed correctly', function(done){
-    var validation = URLService.validateURL(longURLNoHTTP);
+    var validation = URLService.isValidURL(longURLNoHTTP);
     expect(validation).to.be.false;
+    done();
   });
 
 });
 
-describe('URL Redirect CRUD', function(){
-  var redirect = {key: 'abcd', toURL: 'http://test.com/test'};
-  it('saves a test redirect to database', function(done){
+/*
+* Unit Tests
+*/
+describe('URL Service will', function(){
+
+  //test data
+  var redirect = {key: 'testkey', toURL: 'http://test.com/test'};
+
+  it('save the redirect to database', function(done){
     URLService.saveRedirect(redirect, function(err, response){
       expect(err).to.be.null;
       expect(response).to.not.be.null;
@@ -53,19 +60,21 @@ describe('URL Redirect CRUD', function(){
     });
   });
 
-  it('retrieves tests redirect from database', function(done){
+  it('retrieve the redirect from database', function(done){
     URLService.getRedirectForKey(redirect.key, function(err, response){
       expect(err).to.be.null;
-      expect(response.toURL === 'http://test.com/test');
+      expect(response.toURL === redirect.testURL);
+      done();
     });
   })
 
-  it('deletes test redirect from database', function(done){
+  it('deletes the redirect from database', function(done){
     URLService.removeRedirectWithKey(redirect.key, function(err, response){
       expect(err).to.be.null;
       expect(response).to.not.be.null;
       URLService.getRedirectForKey(redirect.key, function(err, response){
-        expect(err).to.not.be.null;
+        expect(response).to.be.null;
+        done();
       });
     });
   });
@@ -78,21 +87,8 @@ describe('URL API', function(){
 
   var testURL = 'http://test.com';
 
-  it('responds to GET /api/url with json data', function(done){
-    var api = '/api/url';
-    request(app)
-    .get('/api/url')
-    .expect(200)
-    .end(function(err, res){
-      expect(err).to.be.null;
-      expect(res).to.not.be.null;
-      expect(res).to.be.json;
-      done();
-    });
-  });
-
-  var shortened;
-  it('responds to POST /api/url with shortened url', function(done){
+  var key; //to be assigned to the key (shortened url) return value from generated redirect
+  it('responds with a shortened url for requests: POST /api/url', function(done){
     request(app)
     .post('/api/url')
     .send({url: testURL})
@@ -100,28 +96,29 @@ describe('URL API', function(){
       expect(err).to.be.null;
       expect(res.statusCode).to.be.equal(201);
       expect(res).to.be.json;
-      expect(res.shortened).to.not.be.null;
-      expect(res.shortened === testURL).to.be.false;
-      //save shortened url for other tests
-      shortened = res.shortened;
+      expect(res.body.key).to.not.be.null;
+      expect(res.body.key === testURL).to.be.false;
+      key = res.body.key; //save the key (shortened url) for use in other tests
       done();
     });
   });
 
-  it('successfully redirects a known shortened url', function(done){
+  //use same key retrived from creation test
+  it('successfully redirects GET /* made with test redirect key', function(done){
     request(app)
-    .get('/' + shortened)
+    .get('/'+ key)
     .end(function(err, res){
       expect(err).to.be.null;
       expect(res.statusCode).to.be.equal(301);
-      //need to implement redirect detection
+      expect(res.header['localhost'] === testURL);
       done();
     });
   });
 
-  it('successfully deletes a shortened url from db', function(done){
+  //use same key retrived from creation test
+  it('successfully deletes a redirection via a supplied key', function(done){
     request(app)
-    .delete('/app/url/' + testURL)
+    .delete('/api/url/' + key)
     .end(function(err, res){
       expect(err).to.be.null;
       expect(res.statusCode).to.be.equal(200);
