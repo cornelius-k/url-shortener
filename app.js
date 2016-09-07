8c41cdc3676701b461a5f8f1444ae8f645eed358
Neil Kempin <neilkempin@gmail.com>
2016-09-06 23:57:31 -0400

    change spacing in app.js

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ var config = require('./config.global');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 
+//configure express
 var app = express();
 exports.app = app;
-
 app.set('view engine', 'jade');
 app.use('/public', express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
@@ -20,15 +20,24 @@ app.use(bodyParser.urlencoded({
    limit: '5mb'
 }));
 app.use(bodyParser.json({limit: '15mb'}));
-
 const PORT = process.env.PORT || 3000;
 
+
 //connect to mongo and start web server
-MongoClient.connect(config.mongoURL + config.dbName, (err, db) =>{
+var db;
+MongoClient.connect(config.mongoURL + config.dbName, (err, database) =>{
   if (err) return console.log(err);
   app.listen(PORT, function() {
     console.log('listening on ', PORT);
   });
+  db = database;
 });
 
+/*
+* Application Routes
+*/
+
 app.get('/', (req, res) => res.send('working'));
+
+//url api routes live in url-api
+require('./api/url-api').addRoutes(app);
